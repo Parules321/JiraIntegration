ebd393e745ba5dc267dafc2f918b1c85d824fd98
parules321 <parules321@gmail.com>
2024-05-01 11:38:31 -0400

    added comments for test cases

--- src/appointment/cancelAppointment.java
+++ src/appointment/cancelAppointment.java
@@ -3,7 +3,14 @@ package appointment;
 public class cancelAppointment {
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
+		
+		
+		//open broswer and enter url
+		//click on cancel appointment button
+		//select appointment to cancel
+		//get confirmation message for cancellation
 
 	}
 
