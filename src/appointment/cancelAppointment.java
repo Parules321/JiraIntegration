575ea7de7db16280d36d89470c0a3ec37a9edcca
parules321 <parules321@gmail.com>
2024-05-04 06:19:11 -0400

    SCRUM-13

--- src/appointment/cancelAppointment.java
+++ src/appointment/cancelAppointment.java
@@ -4,9 +4,7 @@ public class cancelAppointment {
 
 	public static void main(String[] args) {
 		
-	//ajb;oirwo jotyhjojo6oyjort
-		
-		//checking if this integration works with jira
+		//rechecking if this integration works with jira
 		//open broswer and enter url
 		//click on cancel appointment button
 		//select appointment to cancel
